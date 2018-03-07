e53741754d6f6039bc335a82e40af633e8a9762e
Tom Harley <trh@st-andrews.ac.uk>
2018-03-07 13:33:01 +0000

    Add documentation for some files

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,31 @@ import { default as File } from './db/model/File';
 import { default as Project } from './db/model/Project';
 import { default as User } from './db/model/User';
 
+/**
+ * Configuration of the Express server app, including installation of
+ * various middleware functions.
+ */
+
+/**
+ * The Class used to configure the Express app used in the server instance.
+ */
 class App {
+	/**
+	 * The Express app used throught the server.
+	 */
 	public express: ex.Express;
+
+	/**
+	 * Whether or not logging is enabled on the server.
+	 */
 	private readonly logEnabled: boolean = true;
 
+	/**
+	 * Constructs an instance of the app, by loading middleware.
+	 *
+	 * @param enableLog Whether or not logging should be enabled on the
+	 *   server.
+	 */
 	public constructor(enableLog: boolean) {
 		this.logEnabled = enableLog;
 		this.express = ex();
@@ -35,11 +56,13 @@ class App {
 		this.errorware();
 	}
 
-	// Configure Express middleware.
+	/**
+	 * Configures regular Express middleware.
+	 */
 	private middleware(): void {
 		this.express.use(expressJwt({secret: 'Mr Secret'})
 			.unless({path: [
-				'/cs3099group-be-4/login',
+				'/cs3099group-be-4/oauth/token',
 				'/cs3099group-be-4/_supported_protocols_'
 			] })
 		);
@@ -51,7 +74,9 @@ class App {
 		this.express.use(bodyParser.urlencoded({ extended: false }));
 	}
 
-	// Configure API endpoints.
+	/**
+	 * Adds the Router routes to the Express app.
+	 */
 	private mountRoutes(): void {
 		const defRouter: ex.Router = ex.Router();
 		defRouter.get('/', (req: ex.Request, res: ex.Response): void => {
@@ -75,11 +100,22 @@ class App {
 		this.express.use('/', defRouter);
 	}
 
+	/**
+	 * Adds error-handling (4-arity) middleware to the Express app.
+	 */
 	private errorware(): void {
 		this.express.use(unauthorisedErr);
 		this.express.use(errorHandler);
 	}
 
+	/**
+	 * Introduces variable-preconditioning middleware to the Express app.
+	 *
+	 * These middleware functions pre-process Express' matching variables
+	 * in routes (e.g. `:project_name`).
+	 * If an entity cannot be found in the database, the corresponding
+	 * value is set to `null` instead.
+	 */
 	private precondition(): void {
 		// Fetch a user from their name:
 		this.express.param(
@@ -197,6 +233,10 @@ class App {
 }
 
 export default new App(true).express;
+
+/**
+ * A version of the app used only for testing.
+ */
 export function TestApp() {
 	return new App(false).express;
 }
--- src/errors/errorware.ts
+++ src/errors/errorware.ts
@@ -1,5 +1,12 @@
 import { NextFunction,  Request, Response } from 'express';
 
+/**
+ * General error and error-handling logic for the middleware stack.
+ */
+
+/**
+ * The body of the response to a client on errors.
+ */
 export interface ErrorResponseBlock {
 	status: string;
 	error: string;
@@ -9,7 +16,18 @@ export interface ErrorResponseBlock {
 	error_data?: any;
 }
 
+/**
+ * The custom-made Error format, containing enough information to send in
+ * response to a client.
+ */
 export class RequestError extends Error {
+	/**
+	 * Used to check that an object is specifically a RequestError.
+	 *
+	 * @param test The object to test.
+	 * @returns `true` if the object is a [[RequestError]]; `false`
+	 *   otherwise.
+	 */
 	public static is(test: Object): test is RequestError {
 		const err: RequestError = test as RequestError;
 		return err.code !== undefined
@@ -17,11 +35,36 @@ export class RequestError extends Error {
 			&& err.responseBlock !== undefined;
 	}
 
+	/**
+	 * The http response code associated with the error.
+	 */
 	public code: number;
+
+	/**
+	 * The user-friendly message that describes the error.
+	 */
 	public userMessage?: string;
+
+	/**
+	 * Miscellaneous data associated with the error.
+	 */
 	// tslint:disable-next-line:no-any
 	public errorData?: any;
 
+	/**
+	 * Constructs a [[RequestError]] using information intended to be sent
+	 * to the client.
+	 *
+	 * @param errCode The http error-code.
+	 * @param errName The descriptive name of the problem, for further
+	 *   detail from the error-code.
+	 * @param errDescription A more verbose description based on the error
+	 *   name.
+	 * @param errUserMessage A user-friendly version of the description, to
+	 *   possibly be displayed to the user.
+	 * @param errData Miscellaneous data related to the error; probably
+	 *   used more during development.
+	 */
 	public constructor(
 		errCode: number = 500,
 		errName: string = 'internal_server_error',
@@ -48,11 +91,29 @@ export class RequestError extends Error {
 	}
 }
 
+/**
+ * The outside shell of an Express error-handling (_4-arity_) function.
+ *
+ * @param err The Error to act on.
+ * @param req The Express http request.
+ * @param res The Express http response.
+ * @param next The next Express middleware function to use; probably another
+ *   [[Errorware]].
+ */
 export type Errorware =
 	(err: Error, req: Request, res: Response, next: NextFunction) => void;
 
+/**
+ * A terminal [[Errorware]] for sending errors to the user.
+ * Has special functionality for handling [[RequestError]]s, but can handle
+ * regular Errors too.
+ *
+ * @param err The Error to act on.
+ * @param req The Express http request.
+ * @param res The Express http response.
+ */
 export const errorHandler: Errorware =
-	(err: Error, req: Request, res: Response, next: NextFunction): void => {
+	(err: Error, req: Request, res: Response): void => {
 	if (RequestError.is(err)) {
 		res.status(err.code)
 			.json(err.responseBlock());
